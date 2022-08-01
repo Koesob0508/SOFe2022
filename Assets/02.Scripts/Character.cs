137c33b06ea93d69aa10aa821f94f27adacc8cdd
Koesob3706 <epi3706@khu.ac.kr>
2022-08-01 23:25:32 +0900

    GameManager 구현, Character-Enemy,Hero 레이아웃 구현

---
+++ Assets/02.Scripts/Character.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // 접근 한정자를 어떻게 할 것인지, 프로퍼티를 사용할 것인지
+    public float MaxHP;
+    public float CurrentHP;
+    public float Hunger;
+    public float MaxMana;
+    public float MoveSpeed;
+    public float AttackRange;
+    public float AttackSpeed;
+}
