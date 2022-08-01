4a1b2205034038c49604d6a68a9b1d02866990d6
Koesob3706 <epi3706@khu.ac.kr>
2022-08-01 23:10:51 +0900

    레이아웃 구현 중...

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+    private static StageManager stage = null;
+
+    private void Awake()
+    {
+        // 教臂沛 备泅何
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (instance != this)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+    public static GameManager Instance
+    {
+        // 教臂沛 备泅何
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+
+            return instance;
+        }
+    }
+
+}
