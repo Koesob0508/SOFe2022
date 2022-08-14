06c2a240db5c09de300e25c98f8927c06ca6ad01
Def_JH <wlghks1502@gmail.com>
2022-08-14 22:38:04 +0900

    Meeting Change

--- Assets/02.Scripts/CustomSceneManager.cs
+++ Assets/02.Scripts/CustomSceneManager.cs
@@ -35,6 +35,24 @@ public class CustomSceneManager
                 GameObject obj = new GameObject("BattleManager");
                 GameManager.Battle = obj.AddComponent<BattleSceneManager>();
                 GameManager.Battle.Init(GameManager.MapType.Boss);
+                obj.AddComponent<Path.PathManager>();
+
+                Hero h1 = (Hero)GameManager.Instance.ObjectCodex.Find((elem) => { return elem.GUID == 0; });
+                Hero h2 = (Hero)GameManager.Instance.ObjectCodex.Find((elem) => { return elem.GUID == 1; });
+                Hero h3 = (Hero)GameManager.Instance.ObjectCodex.Find((elem) => { return elem.GUID == 12; });
+                Enemy e1 = (Enemy)GameManager.Instance.ObjectCodex.Find((elem) => { return elem.GUID == 100; });
+                Enemy e2 = (Enemy)GameManager.Instance.ObjectCodex.Find((elem) => { return elem.GUID == 100; });
+                Enemy e3 = (Enemy)GameManager.Instance.ObjectCodex.Find((elem) => { return elem.GUID == 100; });
+                List<Hero> hlist = new List<Hero>();
+                List<Enemy> elist = new List<Enemy>();
+                hlist.Add(h1);
+                hlist.Add(h2);
+                hlist.Add(h3);
+                elist.Add(e1);
+                elist.Add(e2);
+                elist.Add(e3);
+                GameManager.Battle.Init(hlist, elist, GameManager.MapType.Boss);
+
                 break;
 
             case "03.TownScene":
