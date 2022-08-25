06c6289de4c619d436c614310325dae6fb41bf9a
Def_JH <wlghks1502@gmail.com>
2022-08-25 19:14:36 +0900

    Inventory_Inital
    TODO
    1. Add IDrag Interface Series in Units Class
    2. Add Menu to Check Status of Hero and Return to Inventory in Scene
    3. Check Hero Status in Inventory

--- Assets/02.Scripts/HeroManager.cs
+++ Assets/02.Scripts/HeroManager.cs
@@ -76,7 +76,7 @@ public class HeroManager : MonoBehaviour
         //    }
         //}
         
-        foreach (Hero hero in GameManager.Instance.ObjectCodex)
+        foreach (Hero hero in GameManager.Instance.ObjectCodex.Values)
         {
             if (hero.GUID == guid)
             {
