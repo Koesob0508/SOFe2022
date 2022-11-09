42aa6f379e2a251e7de497fb8453a9bf1a3dcd48
Koesob <epi3706@khu.ac.kr>
2022-11-09 20:12:41 +0900

    적 배치만 하면 됩니다.

---
+++ Assets/02.Scripts/Stage/StageDataStruct.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageDataStruct
+{
+    public int stage;
+    public GameManager.MapType map;
+    public int step;
+    public int case_;
+    public int ruid;
+    public int count;
+}
