f98d9ef46d923d317f7f1d386c2c6324af2b5ab2
Koesob <epi3706@khu.ac.kr>
2022-11-10 20:03:11 +0900

    랜덤하게 몬스터 스폰 구현

--- Assets/02.Scripts/Stage/StageDataStruct.cs
+++ Assets/02.Scripts/Stage/StageDataStruct.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageDataStruct
+public struct StageData
 {
     public int stage;
     public GameManager.MapType map;
@@ -10,4 +10,30 @@ public class StageDataStruct
     public int case_;
     public int ruid;
     public int count;
+}
+
+public struct StageEnemy
+{
+    public int ruid;
+    public int count;
+}
+
+public class StageCase
+{
+    public List<StageEnemy> enemies = new List<StageEnemy>();
+}
+
+public class StageStep
+{
+    public List<StageCase> cases = new List<StageCase>();
+}
+
+public class StageMapType
+{
+    public List<StageStep> steps = new List<StageStep>();
+}
+
+public class StageLevel
+{
+    public List<StageMapType> mapTypes = new List<StageMapType>();
 }
