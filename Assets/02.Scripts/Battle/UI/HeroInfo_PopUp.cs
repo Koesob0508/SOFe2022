4cd86cf52003031bcf513900164c3becc7052323
Def_JH <ppp1502@naver.com>
2022-11-12 00:54:07 +0900

    Color + Font Refine

--- Assets/02.Scripts/Battle/UI/HeroInfo_PopUp.cs
+++ Assets/02.Scripts/Battle/UI/HeroInfo_PopUp.cs
@@ -24,9 +24,63 @@ public class HeroInfo_PopUp : MonoBehaviour
         heroData = hero;
         NameTXT.text = hero.Name;
         PersonalityTXT.text = hero.MBTI.ToString();
+        Color color = Color.black;
+        switch (hero.MBTI)
+        {
+            case GameManager.MbtiType.INFP:
+                ColorUtility.TryParseHtmlString("#66C8BB", out color);
+                break;
+            case GameManager.MbtiType.ENFP:
+                ColorUtility.TryParseHtmlString("#F5C012", out color);
+                break;
+            case GameManager.MbtiType.INFJ:
+                ColorUtility.TryParseHtmlString("#64ADCB", out color);
+                break;
+            case GameManager.MbtiType.ENFJ:
+                ColorUtility.TryParseHtmlString("#13A5B4", out color);
+                break;
+            case GameManager.MbtiType.INTJ:
+                ColorUtility.TryParseHtmlString("#C3C3C3", out color);
+                break;
+            case GameManager.MbtiType.ENTJ:
+                ColorUtility.TryParseHtmlString("#37373F", out color);
+                break;
+            case GameManager.MbtiType.INTP:
+                ColorUtility.TryParseHtmlString("#91ADB9", out color);
+                break;
+            case GameManager.MbtiType.ENTP:
+                ColorUtility.TryParseHtmlString("#B62135", out color);
+                break;
+            case GameManager.MbtiType.ISFP:
+                ColorUtility.TryParseHtmlString("#C8E682", out color);
+                break;
+            case GameManager.MbtiType.ESFP:
+                ColorUtility.TryParseHtmlString("#F62662", out color);
+                break;
+            case GameManager.MbtiType.ISTP:
+                ColorUtility.TryParseHtmlString("#363E62", out color);
+                break;
+            case GameManager.MbtiType.ESTP:
+                ColorUtility.TryParseHtmlString("#C82F37", out color);
+                break;
+            case GameManager.MbtiType.ISFJ:
+                ColorUtility.TryParseHtmlString("#F4BF59", out color);
+                break;
+            case GameManager.MbtiType.ESFJ:
+                ColorUtility.TryParseHtmlString("#DA5A8B", out color);
+                break;
+            case GameManager.MbtiType.ISTJ:
+                ColorUtility.TryParseHtmlString("#55545A", out color);
+                break;
+            case GameManager.MbtiType.ESTJ:
+                ColorUtility.TryParseHtmlString("#5CA286", out color);
+                break;
+        }
+        PersonalityTXT.color = color;
+
         HealthBar.fillAmount = hero.CurrentHP / (float)hero.MaxHP;
         HungerBar.fillAmount = hero.CurHunger / 100.0f;
-        Image[] m_ItemImages =ItemPanel.GetComponentsInChildren<Image>();
+        Image[] m_ItemImages = ItemPanel.GetComponentsInChildren<Image>();
         //for(int i = 0; i < hero.Items.Count; i++)
         //{
         //    m_ItemImages[i].sprite = GameManager.Data.LoadSprite(hero.Items[i].GUID);
@@ -34,7 +88,14 @@ public class HeroInfo_PopUp : MonoBehaviour
         for (int i = 0; i < 3; i++)
         {
             if (hero.Items[i] != null)
-                m_ItemImages[i].sprite = GameManager.Data.LoadSprite(hero.Items[i].GUID);
+            {
+                m_ItemImages[i + 1].gameObject.SetActive(true);
+                m_ItemImages[i+1].sprite = GameManager.Data.LoadSprite(hero.Items[i].GUID);
+            }
+            else
+            {
+                m_ItemImages[i + 1].gameObject.SetActive(false);
+            }
         }
     }
 
