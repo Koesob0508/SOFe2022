34ce27b1b7cfcc8b175c4c6f7babce5348754c5c
Def_JH <wlghks1502@gmail.com>
2022-11-11 11:22:05 +0900

    Character & HeroInven HotFix

--- Assets/02.Scripts/Battle/Characters/Units.cs
+++ Assets/02.Scripts/Battle/Characters/Units.cs
@@ -177,7 +177,6 @@ public class Units : MonoBehaviour, IDragHandler, IEndDragHandler
         }
     }
     public void PlayGetHitAniamtion()
-
     {
 
         if (!animator.GetCurrentAnimatorStateInfo(0).IsName("GetHit"))
@@ -232,12 +231,8 @@ public class Units : MonoBehaviour, IDragHandler, IEndDragHandler
         if (hit.collider != null)
         {
             HeroInvenItem item = hit.collider.gameObject.GetComponent<HeroInvenItem>();
-            if(item != null)
+            if(item != null && !item.GetIsHeroInInven())
                 item.ReturnToInven(gameObject);
-            //if (item == invenItemUI)
-            //{
-            //    invenItemUI.ReturnToInven();
-            //}
             else
             {
                 if (screenPos.x > Screen.width / 2)
