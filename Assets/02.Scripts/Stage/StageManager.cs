5f49b8a061a31d029c7bb19b79feab538d77b9ba
Koesob <epi3706@khu.ac.kr>
2022-11-21 18:55:03 +0900

    Stage 뜯어고치기 전 대비

--- Assets/02.Scripts/Stage/StageManager.cs
+++ Assets/02.Scripts/Stage/StageManager.cs
@@ -8,7 +8,6 @@ using System.Text;
 public partial class StageManager : MonoBehaviour
 {
     private StageNode currentStageNode;
-    private GameObject canvas;
     public GameObject laneObject;
     private bool isDebugMode = false;
 
@@ -16,6 +15,7 @@ public partial class StageManager : MonoBehaviour
     public StageNode battleNode;
     public StageNode townNode;
     public StageNode eventNode;
+
     [Header("스테이지 시작 갯수와 단계 갯수 설정")]
     public int startCount;
     public int stepCount;
@@ -25,44 +25,23 @@ public partial class StageManager : MonoBehaviour
     private List<Step> stages;
     public List<int> townIndices;
     [SerializeField] private StageMap stageMap;
-    private float screenHeight;
-    private float screenWidth;
-    private float stageNodeScale;
 
     private List<StageData> stageData;
     private List<StageLevel> loadedStageData;
 
     private string saveData;
+    
+    #region Sub Manager
+    private Viewer viewer;
+    #endregion
 
     // 현재는 GameManager에 StageManager가 할당되어 있어야 한다.
     public void Init()
     {
         DontDestroyOnLoad(this);
 
-        #region canvas settings
-        // stageNode들을 Stage 오브젝트의 자식 오브젝트로 둘 예정
-        canvas = GameObject.Find("Stage Canvas");
-
-        if (canvas == null)
-        {
-            canvas = new GameObject { name = "Stage Canvas" };
-        }
-
-        canvas.transform.position = Vector3.zero;
-
-        DontDestroyOnLoad(canvas);
-
-        screenHeight = Camera.main.orthographicSize * 2;
-        screenWidth = screenHeight * Camera.main.aspect;
-        stageNodeScale = screenHeight * 0.1f;
-
-        var stageManagerPosition = this.transform.position;
-        stageManagerPosition.x -= screenWidth * 3 / 8;
-        stageManagerPosition.y -= screenHeight * 3 / 8;
-
-        canvas.transform.position = stageManagerPosition;
-        canvas.SetActive(false);
-        #endregion
+        viewer = new Viewer(this);
+        viewer.Init();
 
         if (saveData != null)
         {
@@ -88,7 +67,7 @@ public partial class StageManager : MonoBehaviour
     {
         saveData = null;
 
-        Destroy(canvas);
+        viewer.Clear();
     }
 
     private void LoadStageData(List<StageData> _stageData)
@@ -198,18 +177,15 @@ public partial class StageManager : MonoBehaviour
 
         seeds = InitSeed(_startCount, _stepCount);
         seeds = RandomizeSeed(seeds, _townIndices);
-        seeds = RandomizePosition(seeds, screenHeight);
+        seeds = RandomizePosition(seeds);
 
-        stages = GenerateNode(seeds, stageNodeScale);
+        stages = viewer.GenerateNode(seeds);
 
         stages = SetPath(stages, seeds);
 
         InitLane(laneObject, stages);
 
         stageMap = new StageMap(stages);
-
-        //string stageMapToString = SerializeStageMap();
-        //SaveStageMap(stageMapToString);
     }
 
     public void SetStageData(List<StageData> _stageData)
@@ -221,9 +197,8 @@ public partial class StageManager : MonoBehaviour
 
     private void LoadStageMap(string _saveData)
     {
-        // Debug.Log("Stage Map Loaded : " + _saveData);
         stageMap = DeserializeStageMap(_saveData);
-        ReconstructStageNodes(stageMap);
+        viewer.ReconstructStageNodes(stageMap);
         ReconstructLane(laneObject, stageMap);
     }
 
@@ -407,7 +382,7 @@ public partial class StageManager : MonoBehaviour
         return randomIndex;
     }
 
-    private List<List<Seed>> RandomizePosition(List<List<Seed>> _seeds, float _screenHeight)
+    private List<List<Seed>> RandomizePosition(List<List<Seed>> _seeds)
     {
         foreach (List<Seed> stepList in _seeds)
         {
@@ -417,7 +392,8 @@ public partial class StageManager : MonoBehaviour
                 // 돌아와서 하면 될 내용
                 // yPosition은 screenHeight / startCount
                 // xPosition은 그것에 1.5배? 정도
-                float yStep = _screenHeight / (startCount + 1);
+                //float yStep = _screenHeight / (startCount + 1);
+                float yStep = 1f;
                 float xStep = yStep * 1.5f;
 
                 // 범위는 딱 yPosition의 절반
@@ -434,46 +410,6 @@ public partial class StageManager : MonoBehaviour
         }
 
         return _seeds;
-    }
-
-    private List<Step> GenerateNode(List<List<Seed>> _seeds, float _nodeScale)
-    {
-        List<Step> resultList = new List<Step>();
-
-        foreach (List<Seed> steps in _seeds)
-        {
-            Step stageSteps = new Step();
-
-            foreach (Seed seed in steps)
-            {
-                Vector2 position = seed.Position + new Vector2(canvas.transform.position.x, canvas.transform.position.y);
-
-                StageNode stageNode = null;
-
-                switch (seed.Type)
-                {
-                    case StageType.Battle:
-                        stageNode = Instantiate(battleNode, position, Quaternion.identity, canvas.transform);
-                        break;
-                    case StageType.Town:
-                        stageNode = Instantiate(townNode, position, Quaternion.identity, canvas.transform);
-                        break;
-                    case StageType.Event:
-                        stageNode = Instantiate(eventNode, position, Quaternion.identity, canvas.transform);
-                        break;
-                }
-
-                stageNode.Init(seed, _nodeScale);
-
-                stageNode.RegistStageNode += GameManager.Stage.UpdateCurrentNode;
-
-                stageSteps.AddStageNode(stageNode);
-            }
-
-            resultList.Add(stageSteps);
-        }
-
-        return resultList;
     }
 
     private List<Step> SetPath(List<Step> _stages, List<List<Seed>> _seeds)
@@ -523,12 +459,12 @@ public partial class StageManager : MonoBehaviour
 
     public void ShowStageMap()
     {
-        canvas.SetActive(true);
+        viewer.On();
     }
 
     public void HideStageMap()
     {
-        canvas.SetActive(false);
+        viewer.Off();
     }
 
     public List<Enemy> GetEnemies()
@@ -563,14 +499,7 @@ public partial class StageManager : MonoBehaviour
     private void UpdateCurrentNode(StageNode _currentNode)
     {
         currentStageNode = _currentNode;
-
-        if (_currentNode.transform.localPosition.x > screenWidth / 2)
-        {
-            float moveX = _currentNode.transform.localPosition.x - screenWidth / 2;
-            var canvasPosition = canvas.transform.position;
-            canvasPosition.x -= moveX;
-            canvas.transform.position = canvasPosition;
-        }
+        viewer.UpdateCanvas(_currentNode.transform.position.x);
     }
 
     private void UnloadStages()
@@ -598,45 +527,16 @@ public partial class StageManager : MonoBehaviour
     {
         saveData = _saveData;
     }
-
-    private void SaveStageMap(string _serializedStageMap)
+    /// <summary>
+    /// 혹시 몰라 쓰진 않았는데 Serialized StageMap을 줘야합니다.
+    /// </summary>
+    /// <param name="_serializedStageMap"></param>
+    private void GenerateSaveFile(string _serializedStageMap)
     {
         FileStream fileStream = new FileStream("Assets/Koesob/Save.json", FileMode.Create);
         byte[] data = Encoding.UTF8.GetBytes(_serializedStageMap);
         fileStream.Write(data, 0, data.Length);
         fileStream.Close();
-    }
-
-    private void ReconstructStageNodes(StageMap _stageMap)
-    {
-        Debug.Log("Stage Node Reconstruct");
-        foreach (Step step in _stageMap.stages)
-        {
-            foreach (SerializedNode node in step.serializeNodes)
-            {
-                Vector2 position = node.position;
-                StageNode stageNode = null;
-
-                switch (node.type)
-                {
-                    case StageType.Battle:
-                        stageNode = Instantiate(battleNode, position, Quaternion.identity, canvas.transform);
-                        break;
-                    case StageType.Town:
-                        stageNode = Instantiate(townNode, position, Quaternion.identity, canvas.transform);
-                        break;
-                    case StageType.Event:
-                        stageNode = Instantiate(eventNode, position, Quaternion.identity, canvas.transform);
-                        break;
-                }
-
-                stageNode.LoadInit(node, stageNodeScale);
-
-                stageNode.RegistStageNode += GameManager.Stage.UpdateCurrentNode;
-
-                step.AddStageNode(stageNode);
-            }
-        }
     }
 
     private void ReconstructLane(GameObject _lanePrefab, StageMap _stageMap)
@@ -673,24 +573,16 @@ public partial class StageManager : MonoBehaviour
 
     public void MoveCanvasLeft()
     {
-        var resultX = canvas.transform.position.x - 2;
-
-        var resultPosition = canvas.transform.position;
-        resultPosition.x = resultX;
-        canvas.transform.position = resultPosition;
+        viewer.MoveLeftCanvas(2);
     }
 
     public void MoveCanvasRight()
     {
-        var resultX = canvas.transform.position.x + 2;
-
-        var resultPosition = canvas.transform.position;
-        resultPosition.x = resultX;
-        canvas.transform.position = resultPosition;
+        viewer.MoveRightCanvas(2);
     }
 
     public string GetLastStage()
     {
-        return null;
+        return currentStageNode.Step.ToString();
     }
 }
